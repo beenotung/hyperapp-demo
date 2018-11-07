add8fa592daa17184d27a60918adc37d9e683002
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-11-07 12:26:37 +0800

    setup the project skeleton with typescript

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import { app, h } from "hyperapp"
+
+(window as any).h = h
+
+const state = {
+  count: 0
+}
+
+const actions = {
+  down: value => state => ({ count: state.count - value }),
+  up: value => state => ({ count: state.count + value })
+}
+
+const view = (state, actions) => (
+  <div>
+    <h1>{state.count}</h1>
+    <button onclick={() => actions.down(1)}>-</button>
+    <button onclick={() => actions.up(1)}>+</button>
+  </div>
+)
+
+app(state, actions, view, document.body)
+
+document.title = "App Ready"
+console.log("ok.")
